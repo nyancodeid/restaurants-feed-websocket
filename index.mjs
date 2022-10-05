aecba56fd3cc633af84ff475a746523db9604465
Ryan Aunur <ryandevstudio@gmail.com>
2022-10-06 06:12:04 +0700

    env(port): made port available on env

--- index.mjs
+++ index.mjs
@@ -3,7 +3,7 @@ import { WebSocketServer } from 'ws';
 
 import { restaurants } from './restaurants.mjs';
 
-const port = 8080;
+const port = process.env.PORT || 80;
 const wss = new WebSocketServer({ port });
 const clients = new Map();
 
